06fb976989c3331b5bf0addaacfabe8c7e0780fb
3arthur6 <arthur.trouillot@gmail.com>
2022-01-30 16:10:34 +0100

    Update support links

--- customize.sh
+++ customize.sh
@@ -66,7 +66,7 @@ patchlib() {
     ui_print "  created in your internal storage to github issue or XDA thread"
     ui_print  " "
     ui_print  "- Opening support webpage in 5 seconds"
-    (sleep 5 && am start -a android.intent.action.VIEW -d https://github.com/Magisk-Modules-Repo/BluetoothLibraryPatcher/blob/master/SUPPORT.md >/dev/null) &
+    (sleep 5 && am start -a android.intent.action.VIEW -d https://github.com/3arthur6/BluetoothLibraryPatcher/blob/master/SUPPORT.md >/dev/null) &
     rm -rf $MODPATH
     abort
   fi
